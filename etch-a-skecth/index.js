1f94db0cedcf68eeba7bf211cd11e654772fab5a
Tim Johnson <62769082+tjohnson009@users.noreply.github.com>
2024-01-07 15:24:41 -0500

    Create setSpacing f(x) to dynamically set the size of the cells

--- etch-a-skecth/index.js
+++ etch-a-skecth/index.js
@@ -1,12 +1,13 @@
 const DOM_ELEMENTS = {
     container: document.querySelector('.container'), 
-    div: '<div class="cell"></div>'
+    div: '<div class="cell"></div>', 
 }
 
-let size = 3; 
+let size = 50; 
 
 function createDivs() {
     for (let i=0; i < size**2; i++) {
+        // create size^2 number of identical divs
         DOM_ELEMENTS.container.insertAdjacentHTML("afterbegin", DOM_ELEMENTS.div); 
     }
 }
@@ -14,14 +15,14 @@ function createDivs() {
 function setSpacing() {
     // get cells
     let cells = Array.from(document.querySelectorAll('.cell')); 
-    // get cell rule
+    //sets the flex Basis (starting size) of each cell to be an equal percentage of the container 
     cells.forEach(el => {
-        el.style.width = `${(size / 100) * 100}%`; 
+        el.style.flexBasis = `${(100/size)}%`; 
         // console.log(el.style.width); 
     })
     // set flex property to be 1 1 (size / 100) * 100
 }
 
-createDivs(); // might need to set the flex basis property dynamically depending on the ratio of size / 100
+createDivs(); // might need to set the flex basis property dynamically depending on the ratio of 100 / size
 setSpacing(); 
 // DOM_ELEMENTS.container.append('<h1>HELLO</h1>'); 

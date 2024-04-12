757df5e39d44cdc03f9ad16ec16ff0fdcb388edb
Tim Johnson <62769082+tjohnson009@users.noreply.github.com>
2024-04-12 17:05:05 -0400

    Add addToLibrary function

--- library/index.js
+++ library/index.js
@@ -1,9 +1,16 @@
+const myLibrary = []; 
+
 function Book(title, author, pages) {
     this.title = title; 
     this.author = author; 
     this.pages = pages; // number 
     this.status = 'not read yet'; 
     this.info = () => {
+        console.log(`${this.title} by ${this.author}, ${this.pages} pages, ${this.status}`)
         return `${this.title} by ${this.author}, ${this.pages} pages, ${this.status}`; 
     } 
+}
+
+function addToLibrary() {
+
 }

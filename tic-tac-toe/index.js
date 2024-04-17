27052a49aa699f8a4788e8cca606faa9cc01f312
Tim Johnson <62769082+tjohnson009@users.noreply.github.com>
2024-04-17 17:37:57 -0400

    Finish checkwin function - begin error searching

--- tic-tac-toe/index.js
+++ tic-tac-toe/index.js
@@ -3,11 +3,15 @@ function Gameboard() { // state of the game board
     // const board = [0,0,0,0,0,0,0,0,0]; 
 
     for (let i = 0; i < 9; i++) {
-        board[i].push(Spot()); // 9 spots for tic-tac-toe - array of spot objects
+        board.push(Spot()); // 9 spots for tic-tac-toe - array of spot objects
     }
 
     const getBoard = () => {
         return board; 
+    }
+
+    const getSpot = (spotNumber) => {
+        return board[spotNumber]; 
     }
 
     const receiveMarker = (spot, player) => { // checks if the move is valid
@@ -22,7 +26,7 @@ function Gameboard() { // state of the game board
             if (openSpots.includes(spot)) {
                 spot.addValue(player.marker); 
             } else {
-                console.log(`That spot is not open.`); 
+                // console.log(`That spot is not open.`); 
             }
         }
     }
@@ -30,10 +34,11 @@ function Gameboard() { // state of the game board
     const logBoard = () => { // console.log the board before UI implementation
         const boardWithValues = board.map(spot => spot.getValue()); 
         console.log(boardWithValues); 
+        return boardWithValues; 
     }
 
     return {
-        getBoard, receiveMarker, logBoard
+        getBoard, receiveMarker, logBoard, getSpot
     }
 }
 
@@ -45,6 +50,7 @@ function Spot() {
             value = playerMarker; 
         } else {
             console.log('That spot is not open.'); 
+            return 'switch turns'; 
         }
     }
 
@@ -57,7 +63,7 @@ function Spot() {
     }
 }
 
-function GameControl(playerOneName = 'Player One', playerTwoName = "Player Two") {
+function GameControl(playerOneName = 'Player 1', playerTwoName = "Player 2") {
 
     const board = Gameboard(); 
     const players = [
@@ -71,33 +77,68 @@ function GameControl(playerOneName = 'Player One', playerTwoName = "Player Two")
         }
     ]; 
 
-    let whoseTurn = players[0]; 
+    let activePlayer = players[0];  // player object
 
     const switchTurns = () => {
-        whoseTurn = whoseTurn === players[0] ? players[1] : players[0]; 
+        activePlayer = activePlayer === players[0] ? players[1] : players[0]; 
     }
 
     const getWhoseTurn = () => {
-        return whoseTurn; 
+        return activePlayer; // player object
     }
 
     const restartGame = () => {
 
     }
 
-    const playRound =  (spot) => { // spot is a number from 0 - 9
-        board[spot].changeValue(whoseTurn.marker); 
-        board.receiveMarker(board[spot], whoseTurn); 
-        board.logBoard(); 
-        switchTurns(); 
+    const playRound = (spotNumber) => { // spot is a number from 0 - 9
+        if (board.getBoard()[spotNumber].getValue() === 0) { // forces selection to only open spots
+            console.log(`${getWhoseTurn().name} chose spot ${spotNumber}.`);  // no semi colon here breaks the app
+                // board.logBoard(); 
+                (board.getBoard())[spotNumber].changeValue(activePlayer.marker); 
+                board.receiveMarker(board[spotNumber], activePlayer); 
+                board.logBoard(); 
+                switchTurns(); 
+                checkForWin(); 
+            } else {                
+                console.log('That spot is not open or invalid.'); 
+                switchTurns(); // resets the players turn to try again
+        }
     }
 
     const checkForWin = () => {
-        // start checking at 5 spots filled in the array because it's not possible to have a winner before that
-        const possibleWins = []; 
+        let nonZeroSpots = board.getBoard().filter(spot => (spot.getValue() !== 0)); 
+        if (nonZeroSpots.length >= 5) { // start checking for wins once 5 spots are filled - the lowest possible number
+            console.log('Checking for win'); 
+        // let whoWentFirst = whoseTurn; 
+        const possibleWins = [ // winning combos
+        [0, 1, 2],
+        [0, 4, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 4, 6],
+        [2, 5, 8],
+        [3, 4, 5],
+        [6, 7, 8]
+    ]; 
+            // take the spots on the board and check them against the possible wins - for loop because cannot break forEach
+        for (let i = 0; i < possibleWins.length; i++) { // for every possible win
+            // for(let j = 0; j < possibleWins[i].length; j++) {
+            if (possibleWins[i].every(spot => board[spot].getValue() === 1)) { // playerOne occupies every spot in possible win combo
+                console.log('Player One wins'); 
+                break; 
+            } else if (possibleWins[i].every(spot => board[spot].getValue() === 2)) { // playerTwo occupies every spot in possible win combo
+                console.log('Player Two wins');
+                break; 
+            }
+            // }
+        }
+        } 
     }
 
     return {
-
+        checkForWin, getWhoseTurn, switchTurns, playRound
     }
 }
+
+const game = GameControl(); 

709c346fa3529f113d5ecdfd65b85ef7a6951c72
Tim Johnson <62769082+tjohnson009@users.noreply.github.com>
2024-04-30 14:58:13 -0400

    Import fonts; Finish styling of About.JS page

--- beckys-bakery/src/about.js
+++ beckys-bakery/src/about.js
@@ -5,40 +5,55 @@ export default function about() {
 
     let bakerDiv = document.createElement('div');
     bakerDiv.classList.add('bakerDiv'); 
+
     let bakerH1 = document.createElement('h1'); 
     bakerH1.innerHTML = 'About Becky'; 
     bakerDiv.appendChild(bakerH1); 
-    let bakerText = bakerDiv.appendChild(document.createElement('div')); 
-    bakerText.classList.add('text'); 
-    let bakerDescription = bakerDiv.appendChild(document.createElement('p')); 
+
+    // let bakerTextDiv = bakerDiv.appendChild(document.createElement('div')); 
+    let bakerTextDiv = document.createElement('div'); 
+    bakerTextDiv.classList.add('text'); 
+    bakerDiv.appendChild(bakerTextDiv); 
+    let bakerDescription = document.createElement('p'); 
     bakerDescription.classList.add('description'); 
+    bakerTextDiv.appendChild(bakerDescription); 
     bakerDescription.innerHTML = `Becky J, the founder and head baker of Becky's Bakery, has been perfecting her craft for over a decade. With a passion for baking that was instilled in her from a young age, she honed her skills in her family's kitchen, experimenting with recipes and techniques until she found the perfect balance of flavors and textures. Today, her unwavering dedication to quality and her commitment to using only the freshest, locally-sourced ingredients are evident in every item that leaves the Becky's Bakery ovens. Her signature creations, from the flaky, buttery croissants to the rich, decadent cakes, have earned her bakery a loyal following throughout the community and beyond. As the heart and soul of Becky's Bakery, she takes great pride in sharing her love of baking with each and every customer who walks through the door.`; 
-    let bakerPictureDiv = bakerDiv.appendChild(document.createElement('div')); 
-    let bakerPicture = new Image(200); 
+    
+    let bakerPictureDiv = document.createElement('div'); 
+    bakerPictureDiv.classList.add('picture-div'); 
+    bakerDiv.appendChild(bakerPictureDiv); 
+    let bakerPicture = new Image(); 
     bakerPicture.classList.add('baker-picture'); 
-    bakerPicture.src = './src/img/andy-sartori-Lmpx1n-TVjk-unsplash.jpg';
+    bakerPicture.src = '../src/img/andy-sartori-Lmpx1n-TVjk-unsplash.jpg';
     bakerPictureDiv.appendChild(bakerPicture); 
     container.appendChild(bakerDiv); 
     
     let bakeryDiv = document.createElement('div');
     bakeryDiv.classList.add('bakery'); 
+
     let bakeryH1 = document.createElement('h1'); 
-    bakeryH1.innerHTML = 'The Bakery'; 
+    bakeryH1.innerHTML = 'About The Bakery'; 
     bakeryDiv.appendChild(bakeryH1); 
-    let bakeryText = bakeryDiv.appendChild(document.createElement('div')); 
-    bakeryText.classList.add('text'); 
-    let bakeryPictureDiv =  bakerDiv.appendChild(document.createElement('div')); 
-    bakeryPictureDiv.classList.add('picture'); 
-    let bakeryPicture = new Image(200); 
-    bakeryPicture.src = './src/img/gabriella-clare-marino-NaGx1KQA-MM-unsplash.jpg';
+
+    
+    let bakeryPictureDiv =  document.createElement('div'); 
+    bakeryPictureDiv.classList.add('picture-div'); 
+    bakeryDiv.appendChild(bakeryPictureDiv); 
+    let bakeryPicture = new Image(); 
+    bakeryPicture.src = '../src/img/gabriella-clare-marino-NaGx1KQA-MM-unsplash.jpg';
     bakeryPicture.classList.add('picture'); 
     bakeryPictureDiv.appendChild(bakeryPicture); 
-    let bakeryDescription =  bakerDiv.appendChild(document.createElement('p')); 
+    
+    let bakeryText = document.createElement('div'); 
+    bakeryText.classList.add('text'); 
+    let bakeryDescription =  document.createElement('p'); 
     bakeryDescription.classList.add('description'); 
-    bakeryDescription.innerHTML = `What started as a small, home-based baking operation in Becky's cozy kitchen has blossomed into a beloved community staple. Over 20 years ago, Becky began selling her homemade breads and pastries at the local farmer's market, quickly earning a loyal following with her uncompromising commitment to quality and her warm, welcoming demeanor. Word of Becky's exceptional baking skills soon spread throughout the neighborhood, and it wasn't long before she outgrew her home kitchen. With the support of her family, Becky opened the doors to Becky's Bakery, a charming storefront that has since become a gathering place for locals and a destination for visitors seeking a taste of the community's culinary heart. Today, Becky's Bakery stands as a testament to the power of passion, perseverance, and a genuine love for bringing people together through the universal joy of freshly baked goods.`
+    bakeryDescription.innerHTML = `What started as a small, home-based baking operation in Becky's cozy kitchen has blossomed into a beloved community staple. Over 20 years ago, Becky began selling her homemade breads and pastries at the local farmer's market, quickly earning a loyal following with her uncompromising commitment to quality and her warm, welcoming demeanor. Word of Becky's exceptional baking skills soon spread throughout the neighborhood, and it wasn't long before she outgrew her home kitchen. With the support of her family, Becky opened the doors to Becky's Bakery, a charming storefront that has since become a gathering place for locals and a destination for visitors seeking a taste of the community's culinary heart. The bakery's cozy atmosphere, with its inviting aromas and friendly staff, has made it a true neighborhood gem. Becky's hands-on approach and dedication to using only the finest, locally-sourced ingredients have ensured that every baked good is a labor of love. Today, Becky's Bakery stands as a testament to the power of passion, perseverance, and a genuine love for bringing people together through the universal joy of freshly baked goods. It is a place where memories are made and traditions are celebrated, one delicious bite at a time.`
+    // bakeryText.appendChild(bakeryText); 
+    bakeryText.appendChild(bakeryDescription); 
+    bakeryDiv.appendChild(bakeryText); 
+
     container.appendChild(bakeryDiv); 
     
-    
-
-
+    return container; 
 }
--- beckys-bakery/src/index.js
+++ beckys-bakery/src/index.js
@@ -1,36 +1,58 @@
-console.log('We are live and in color.'); 
-
+// console.log('We are live and in color.'); 
+import about from './about'; 
+import './style.css'; 
 // let divCreator = (divName, className) => {
 //     divName = document.createElement('div'); 
 //     divName.classList.add(className); 
 //     return divName; 
 // }
+// function index() {
 
-let contentDiv = document.querySelector('#content'); 
-let hero = document.createElement('div'); 
-hero.classList.add('hero');
-let heroText = document.createElement('div'); 
-heroText.classList.add('hero-text'); 
-
-contentDiv.appendChild(hero); 
-contentDiv.appendChild(heroText); 
-const heroImage = new Image(1500); 
-heroImage.src = '../src/img/mae-mu-kID9sxbJ3BQ-unsplash.jpg'; 
-heroImage.alt = 'A basket of baked chocolate chip cookies'; 
-console.log(heroImage); 
-hero.insertAdjacentElement('beforeend', heroImage); // NOT insertAdjacentHTML because we created an element, not a string representation of an element
-hero.insertAdjacentHTML('beforeend', '<h1>A Collection Of Custom-Made Culinary Creations</h1>'); 
-heroText.insertAdjacentHTML('beforeend', "<p>Welcome to Becky's Bakery, where the aroma of freshly baked goods fills the air and the taste of homemade goodness delights the senses. For over 10 years, our family-owned bakery has been a beloved destination for locals and visitors alike. Offering a wide selection of artisanal breads, decadent pastries, and mouthwatering desserts, our bakery exists to please your taste buds and the craving in your heart.</p>")
-heroText.insertAdjacentHTML('beforeend', "<p>At Becky's Bakery, we take pride in using only the finest, locally-sourced ingredients to create our signature baked goods. From our flaky croissants and buttery scones to our rich, fudgy brownies and delicate macarons, every item is crafted with care and attention to detail. Whether you're grabbing a quick breakfast on the go or indulging in an afternoon treat, we are always on hand to ensure your experience is nothing short of exceptional. Visit us today and discover why Becky's Bakery has become a cherished part of the community.</p>")
-
-let switchTabs = () => {
-    let children = Array.from(document.querySelector('#content').children); 
-    console.log(children); 
+    let contentDiv = document.querySelector('#content'); 
+    let hero = document.createElement('div'); 
+    hero.classList.add('hero');
+    let heroText = document.createElement('div'); 
+    heroText.classList.add('hero-text'); 
+    
+    contentDiv.appendChild(hero); 
+    contentDiv.appendChild(heroText); 
+    const heroImage = new Image(1500); 
+    heroImage.src = '../src/img/mae-mu-kID9sxbJ3BQ-unsplash.jpg'; 
+    heroImage.alt = 'A basket of baked chocolate chip cookies'; 
+    console.log(heroImage); 
+    // hero.insertAdjacentElement('beforeend', heroImage); // NOT insertAdjacentHTML because we created an element, not a string representation of an element
+    hero.insertAdjacentHTML('beforeend', `<h1>Becky's Bakery</h1>`); 
+    hero.insertAdjacentHTML('beforeend', '<h2>A Collection Of Custom-Made Culinary Creations</h2>'); 
+    heroText.insertAdjacentHTML('beforeend', "<p>Welcome to Becky's Bakery, where the aroma of freshly baked goods fills the air and the taste of homemade goodness delights the senses. For over 10 years, our family-owned bakery has been a beloved destination for locals and visitors alike. Offering a wide selection of artisanal breads, decadent pastries, and mouthwatering desserts, our bakery exists to please your taste buds and the craving in your heart.</p>")
+    heroText.insertAdjacentHTML('beforeend', "<p>At Becky's Bakery, we take pride in using only the finest, locally-sourced ingredients to create our signature baked goods. From our flaky croissants and buttery scones to our rich, fudgy brownies and delicate macarons, every item is crafted with care and attention to detail. Whether you're grabbing a quick breakfast on the go or indulging in an afternoon treat, we are always on hand to ensure your experience is nothing short of exceptional. Visit us today and discover why Becky's Bakery has become a cherished part of the community.</p>")
+    // {
+    //     hero, heroText
+    // }; 
+// }
+    
+    let switchTabs = (e) => {
+        console.log(e); 
+        let children = Array.from(document.querySelector('#content').children); 
+        // console.log(children); 
     children.forEach(child => child.remove()); 
+
+    // render the js based on which button is clicked
+    if (e.target.textContent === 'About') {
+        contentDiv.appendChild(about()); 
+    } else if (e.target.textContent === 'Recipes') {
+        contentDiv.appendChild(recipes()); 
+    } else if (e.target.textContent === 'Catering') {
+        contentDiv.appendChild(catering()); 
+    } else if (e.target.textContent === 'Newsletter') {
+        contentDiv.appendChild(newsletter()); 
+    } else {
+        contentDiv.appendChild(hero); 
+        contentDiv.appendChild(heroText); 
+    }
 }
 
 let buttons = Array.from(document.querySelectorAll('button')); 
 buttons.forEach(btn => {
-    btn.addEventListener('click', switchTabs); 
+    btn.addEventListener('click', e => switchTabs(e)); 
 })
 // export {divCreator}; 
